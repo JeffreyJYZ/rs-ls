db9cd970659254667127feadf88d5fe7a201b43b
Jeffrey <yizhoujiang11@gmail.com>
2026-05-20 08:15:57 +0400

    added readme and added more icons

---
+++ README.md
@@ -0,0 +1,11 @@
+# rs-ls
+
+![rs-ls in action](./public/in-action.png)
+![rs-ls in action 2](./public/in-action1.png)
+![rs-ls in action 3](./public/in-action2.png)
+
+I built this as a side project, which is bsically lsd but with background colors made possible by the colored crate.
+
+If there are any extensions you would like to add, or if you want to change the color of a certain logo, or any other general improvements please open a PR.
+
+Thank you for your support!
--- src/main.rs
+++ src/main.rs
@@ -16,22 +16,59 @@ fn main() -> std::io::Result<()> {
         if name.starts_with(".") && !(args().any(|arg| arg == "-a")) {
             continue;
         }
-        let pathext = name.split(".").last().unwrap_or("");
+        let fileext = name.split(".").last().unwrap_or("");
         if path.is_dir() {
             println!("-   {}", name.blue());
             return Ok(());
         }
-        let icon = match pathext {
+        let icon = match fileext {
             "ts" => " ".on_bright_blue(),
             "rs" => " ".on_red(),
             "js" | "mjs" | "cjs" => " ".on_bright_yellow(),
-            "html" => " ".on_bright_red(),
-            "kt" | "ktc" => " ".on_bright_magenta().bright_blue(),
+            "html" | "htm" => " ".on_bright_red(),
+            "kt" | "kts" => " ".on_bright_magenta().bright_blue(),
             "java" => " ".on_red(),
             "tsx" | "jsx" => " ".on_blue(),
-            "json" | "toml" | "yaml" | "yml" => " ".on_black().bright_yellow(),
-            "py" => " ".on_blue().bright_yellow(),
+            "json" | "toml" | "yaml" | "yml" | "jsonc" | "xml" => " ".on_black().bright_yellow(),
+            "py" | "pyi" => " ".on_blue().bright_yellow(),
             "db" | "sql" => " ".on_black().bright_yellow(),
+            "c" | "h" => " ".on_bright_yellow(),
+            "cpp" | "c++" | "cc" | "cxx" | "hpp" | "hh" => " ".on_bright_blue(),
+            "cs" => "󰌛 ".on_purple(),
+            "swift" => " ".on_red().bright_blue(),
+            "go" => " ".on_red().bright_green(),
+            "zig" => " ".on_bright_yellow().on_black(),
+            "php" => " ".on_blue(),
+            "rb" => " ".on_bright_red(),
+            "dart" => " ".on_bright_blue(),
+            "r" => "󰟔 ".bright_red().on_bright_blue(),
+            "pl" => " ".blue().on_white(),
+            "lua" => " ".bright_red().on_green(),
+            "sh" | "bash" | "zsh" | "ps1" | "cmd" | "fish" => " ".black().on_bright_white(),
+            "hs" => " ".on_bright_green(),
+            "ml" | "mli" => " ".bright_yellow().on_black(),
+            "css" => " ".on_bright_red(),
+            "sass" | "scss" => " ".on_red(),
+            "less" => " ".on_yellow().bright_black(),
+            "md" | "mdx" => " ".on_bright_blue(),
+            "svg" | "png" | "jpg" | "jpeg" | "gif" | "webp" | "ico" | "avif" | "heic" => {
+                " ".on_bright_white().bright_black()
+            }
+            "vue" => "󰡄 ".on_bright_cyan(),
+            "svelte" => " ".on_bright_red(),
+            "github" | "git" | "gitignore" | "gitattributes" => " ".on_bright_red(),
+            "nix" => " ".on_bright_yellow(),
+            "zip" | "tar" | "gz" | "rar" | "xz" | "7z" | "tgz" => {
+                " ".on_bright_black().bright_yellow()
+            }
+            "mp4" | "mkv" | "mov" | "webm" => " ".on_bright_black().bright_blue(),
+            "wav" | "mp3" | "ogg" | "flac" => " ".on_bright_black().bright_green(),
+            "pdf" => " ".on_bright_red(),
+            "txt" | "rtf" => " ".on_bright_white().bright_black(),
+            "doc" | "docx" => " ".on_bright_blue(),
+            "ppt" | "pptx" => " ".on_bright_red(),
+            "xls" | "xlsx" => " ".on_bright_green(),
+            "ttf" | "otf" | "woff" | "woff2" => " ".on_bright_black(),
             _ => " ".on_white(),
         };
         println!("- {} {}", icon, name);
